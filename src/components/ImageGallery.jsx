249cb8607b97091cd698935efb1dca4aa68c7ce6
Akash Kumar <73817248+Akash581999@users.noreply.github.com>
2024-06-13 11:29:54 +0530

    Last commit on 13/6/24

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -8,16 +8,19 @@ import Dev4 from "../images/userimg4.png";
 const images = [
   {
     url: "https://picsum.photos/2400/800",
+    // url: "../covers/bgcover1.jpg",
     title: "First Slide",
     description: "Some description for the first slide",
   },
   {
     url: "https://picsum.photos/2400/800",
+    // url: "../covers/bgcover2.jpg",
     title: "Second Slide",
     description: "Some description for the second slide",
   },
   {
     url: "https://picsum.photos/2400/800",
+    // url: "../covers/bgcover3.jpg",
     title: "Third Slide",
     description: "Some description for the third slide",
   },
@@ -48,16 +51,25 @@ const ImageGallery = (props) => {
             >
               <div className="accordion-body">
                 <p className="lead mb-4">
-                  SourceDotCom Pvt Ltd is a leading technology company based in
-                  Gurgaon, Haryana, India. We specialize in providing innovative
-                  solutions in the field of software development, web design,
-                  and digital marketing. Our team consists of highly skilled
-                  professionals who are dedicated to delivering high-quality
-                  products and services to our clients. At SourceDotCom, we
-                  strive to exceed our clients' expectations and contribute to
-                  their success by leveraging the latest technologies and best
-                  practices in the industry. Thank you for considering us for
-                  your technology needs.
+                  Introducing AllTraxs, your ultimate destination for all things
+                  music. AllTraxs offers an unparalleled music streaming
+                  experience, combining a vast library of songs from all genres
+                  with personalized recommendations tailored just for you. With
+                  AllTraxs, you can create custom playlists, discover new
+                  artists and tracks, and explore curated playlists crafted by
+                  music experts. Our seamless interface makes it easy to
+                  navigate and find exactly what you're looking for, whether
+                  it's the latest chart-toppers or hidden gems waiting to be
+                  uncovered. One of AllTraxs's standout features is its robust
+                  recommendation system, which analyzes your listening habits to
+                  suggest music you're sure to love. Whether you're into indie
+                  rock, hip-hop, jazz, or anything in between, AllTraxs has
+                  something for everyone. Plus, with offline listening
+                  capabilities, you can take your music with you wherever you
+                  go, even when you're offline. And with high-quality audio
+                  streaming, you'll enjoy crystal-clear sound that brings your
+                  favorite songs to life like never before. Join the AllTraxs
+                  community today and experience the future of music streaming.
                 </p>
               </div>
             </div>
