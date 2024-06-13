cf77aadbccdb19a6b66e1c8281faa75c709dd04f
Akash Kumar <73817248+Akash581999@users.noreply.github.com>
2024-06-13 11:14:41 +0530

    Last commit on 13/6/24

--- src/components/ImageGallery.jsx
+++ src/components/ImageGallery.jsx
@@ -5,23 +5,19 @@ import Dev2 from "../images/userimg2.jpg";
 import Dev3 from "../images/userimg3.png";
 import Dev4 from "../images/userimg4.png";
 
-
 const images = [
   {
     url: "https://picsum.photos/2400/800",
-    // url: "../covers/bgcover.jpg",
     title: "First Slide",
     description: "Some description for the first slide",
   },
   {
     url: "https://picsum.photos/2400/800",
-    // url: "../covers/bgcover1.jpg",
     title: "Second Slide",
     description: "Some description for the second slide",
   },
   {
     url: "https://picsum.photos/2400/800",
-    // url: "../covers/bgcover2.jpg",
     title: "Third Slide",
     description: "Some description for the third slide",
   },
@@ -29,61 +25,141 @@ const images = [
 
 const ImageGallery = (props) => {
   return (
-    <>
-      <div className={`bg-${props.mode}`}>
-        <Slider images={images} />
-        <div className="container mt-5">
-          <h1 className="mb-4">About Us</h1>
-          <p className="lead mb-4">SourceDotCom Pvt Ltd is a leading technology company based in Gurgaon, Haryana, India. We specialize in providing innovative solutions in the field of software development, web design, and digital marketing. Our team consists of highly skilled professionals who are dedicated to delivering high-quality products and services to our clients. At SourceDotCom, we strive to exceed our clients' expectations and contribute to their success by leveraging the latest technologies and best practices in the industry. Thank you for considering us for your technology needs.</p>
-          <div className="row">
-            <div className="col-md-3 mb-4">
-              <div className="card">
-                <img src={Dev1} className="card-img-top" alt="Developer 1" />
-                <div className="card-body">
-                  <h5 className="card-title">John Doe</h5>
-                  <p className="card-text">Frontend Developer</p>
-                  <p className="card-text">Role: UI/UX design, Frontend development</p>
-                  <p className="card-text">Location: Gurgaon, India</p>
-                </div>
+    <div className={`bg-${props.mode}`}>
+      <div className="container pt-3">
+        <div className="accordion" id="accordionExample">
+          <div className="accordion-item">
+            <div className="accordion-header">
+              <button
+                className="accordion-button"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseOne"
+                aria-expanded="true"
+                aria-controls="collapseOne"
+              >
+                <h1 className="my-2">About Us</h1>
+              </button>
+            </div>
+            <div
+              id="collapseOne"
+              className="accordion-collapse collapse show"
+              data-bs-parent="#accordionExample"
+            >
+              <div className="accordion-body">
+                <p className="lead mb-4">
+                  SourceDotCom Pvt Ltd is a leading technology company based in
+                  Gurgaon, Haryana, India. We specialize in providing innovative
+                  solutions in the field of software development, web design,
+                  and digital marketing. Our team consists of highly skilled
+                  professionals who are dedicated to delivering high-quality
+                  products and services to our clients. At SourceDotCom, we
+                  strive to exceed our clients' expectations and contribute to
+                  their success by leveraging the latest technologies and best
+                  practices in the industry. Thank you for considering us for
+                  your technology needs.
+                </p>
               </div>
             </div>
-            <div className="col-md-3 mb-4">
-              <div className="card">
-                <img src={Dev2} className="card-img-top" alt="Developer 2" />
-                <div className="card-body">
-                  <h5 className="card-title">Jane Smith</h5>
-                  <p className="card-text">Backend Developer</p>
-                  <p className="card-text">Role: Backend development, Database management</p>
-                  <p className="card-text">Location: Gurgaon, India</p>
-                </div>
-              </div>
+          </div>
+          <div className="accordion-item">
+            <div className="accordion-header">
+              <button
+                className="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseTwo"
+                aria-expanded="false"
+                aria-controls="collapseTwo"
+              >
+                <h1 className="my-2">Our Team</h1>
+              </button>
             </div>
-            <div className="col-md-3 mb-4">
-              <div className="card">
-                <img src={Dev3} className="card-img-top" alt="Developer 3" />
-                <div className="card-body">
-                  <h5 className="card-title">David Johnson</h5>
-                  <p className="card-text">Full Stack Developer</p>
-                  <p className="card-text">Role: Frontend and Backend development, Deployment</p>
-                  <p className="card-text">Location: Gurgaon, India</p>
-                </div>
-              </div>
-            </div>
-            <div className="col-md-3 mb-4">
-              <div className="card">
-                <img src={Dev4} className="card-img-top" alt="Developer 4" />
-                <div className="card-body">
-                  <h5 className="card-title">Emily Brown</h5>
-                  <p className="card-text">UI/UX Designer</p>
-                  <p className="card-text">Role: Visual design, User experience optimization</p>
-                  <p className="card-text">Location: Gurgaon, India</p>
+            <div
+              id="collapseTwo"
+              className="accordion-collapse collapse"
+              data-bs-parent="#accordionExample"
+            >
+              <div className="accordion-body">
+                <div className="row">
+                  <div className="col-md-3 mb-4">
+                    <div className="card">
+                      <img
+                        src={Dev1}
+                        className="card-img-top"
+                        alt="Developer 1"
+                      />
+                      <div className="card-body">
+                        <h5 className="card-title">Akash Kumar</h5>
+                        <p className="card-text">Frontend Developer</p>
+                        <p className="card-text">
+                          Role: UI/UX design, Frontend development
+                        </p>
+                        <p className="card-text">Location: Gurgaon, India</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-md-3 mb-4">
+                    <div className="card">
+                      <img
+                        src={Dev2}
+                        className="card-img-top"
+                        alt="Developer 2"
+                      />
+                      <div className="card-body">
+                        <h5 className="card-title">Gourav Rattan</h5>
+                        <p className="card-text">Backend Developer</p>
+                        <p className="card-text">
+                          Role: Backend development, Database management
+                        </p>
+                        <p className="card-text">Location: Gurgaon, India</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-md-3 mb-4">
+                    <div className="card">
+                      <img
+                        src={Dev3}
+                        className="card-img-top"
+                        alt="Developer 3"
+                      />
+                      <div className="card-body">
+                        <h5 className="card-title">Akash Saini</h5>
+                        <p className="card-text">Full Stack Developer</p>
+                        <p className="card-text">
+                          Role: Frontend and Backend development, Deployment
+                        </p>
+                        <p className="card-text">Location: Gurgaon, India</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-md-3 mb-4">
+                    <div className="card">
+                      <img
+                        src={Dev4}
+                        className="card-img-top"
+                        alt="Developer 4"
+                      />
+                      <div className="card-body">
+                        <h5 className="card-title">Kapil Yadav</h5>
+                        <p className="card-text">UI/UX Designer</p>
+                        <p className="card-text">
+                          Role: Visual design, User experience optimization
+                        </p>
+                        <p className="card-text">Location: Gurgaon, India</p>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </>
+      <div className="py-3">
+        <Slider images={images} />
+      </div>
+    </div>
   );
 };
 
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,73 +1,65 @@
 import React from "react";
 
-const Slider = ({ images }, props) => {
+const Slider = ({ images, mode }) => {
   return (
-    <>
-      <div className={`bg-${props.mode}`}>
-        <div className="container border-success">
-          <div
-            id="carouselExampleCaptions"
-            className="carousel carousel-dark slide"
-            data-bs-ride="carousel"
-          >
-            <div className="carousel-indicators">
-              {images.map((image, index) => (
-                <button
-                  key={index}
-                  type="button"
-                  data-bs-target="#carouselExampleCaptions"
-                  data-bs-slide-to={index}
-                  className={index === 0 ? "active" : ""}
-                  aria-label={`Slide ${index + 1}`}
-                ></button>
-              ))}
-            </div>
-            <div className="carousel-inner">
-              {images.map((image, index) => (
-                <div
-                  key={index}
-                  className={`carousel-item ${index === 0 ? "active" : ""}`}
-                >
-                  <img
-                    className="musicCover rounded"
-                    src={image.url}
-                    alt={`Slide ${index + 1}`}
-                  />
-                  <div className="carousel-caption d-none d-md-block">
-                    <h5>{image.title}</h5>
-                    <p>{image.description}</p>
-                  </div>
-                </div>
-              ))}
-            </div>
-            <button
-              className="carousel-control-prev"
-              type="button"
-              data-bs-target="#carouselExampleCaptions"
-              data-bs-slide="prev"
-            >
-              <span
-                className="carousel-control-prev-icon"
-                aria-hidden="true"
-              ></span>
-              <span className="visually-hidden">Previous</span>
-            </button>
-            <button
-              className="carousel-control-next"
-              type="button"
-              data-bs-target="#carouselExampleCaptions"
-              data-bs-slide="next"
-            >
-              <span
-                className="carousel-control-next-icon"
-                aria-hidden="true"
-              ></span>
-              <span className="visually-hidden">Next</span>
-            </button>
+    <div className={`bg-${mode}`}>
+      <div className="container border-success">
+        <div
+          id="carouselExampleCaptions"
+          className="carousel carousel-dark slide"
+          data-bs-ride="carousel"
+        >
+          <div className="carousel-indicators rounded">
+            {images.map((image, index) => (
+              <button
+                key={index}
+                type="button"
+                data-bs-target="#carouselExampleCaptions"
+                data-bs-slide-to={index}
+                className={index === 0 ? "active" : ""}
+                aria-label={`Slide ${index + 1}`}
+              />
+            ))}
           </div>
+          <div className="carousel-inner rounded">
+            {images.map((image, index) => (
+              <div
+                key={index}
+                className={`carousel-item ${index === 0 ? "active" : ""}`}
+              >
+                <img
+                  className="musicCover rounded"
+                  src={image.url}
+                  alt={`Slide ${index + 1}`}
+                />
+                <div className="carousel-caption d-none d-md-block">
+                  <h5>{image.title}</h5>
+                  <p>{image.description}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide="prev"
+          >
+            <span className="visually-hidden">Previous</span>
+            <i className="fas fa-chevron-left fa-2x"></i>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide="next"
+          >
+            <span className="visually-hidden">Next</span>
+            <i className="fas fa-chevron-right fa-2x"></i>
+          </button>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
