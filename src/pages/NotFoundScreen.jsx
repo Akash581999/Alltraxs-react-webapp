8c4a1fae50e23d025354e4ca19a9680af07fb0bc
Akash Kumar <73817248+Akash581999@users.noreply.github.com>
2024-06-20 09:43:39 +0530

    Fixed not found page

--- src/pages/NotFoundScreen.jsx
+++ src/pages/NotFoundScreen.jsx
@@ -1,16 +1,19 @@
 import React from "react";
-import notfound from "../images/bgimgnotfound.jpg";
+// import notfound from "../images/bgimgnotfound.jpg";
 
 const NotFoundScreen = () => {
   return (
-    <div className="container d-flex flex-column justify-content-center align-items-center">
-      {/* <h1 className="text-danger">Error 404</h1>
-      <h2 className="text-dark">Requested page not found</h2> */}
-      <img
+    <div
+      className="d-flex flex-column justify-content-center align-items-center"
+      style={{ height: "100vh", backgroundColor: "rgb(139, 8, 255)" }}
+    >
+      <h1 className="text-danger">Error 404</h1>
+      <h2 className="text-light">Requested page not found....</h2>
+      {/* <img
         src={notfound}
         alt="bgimgnotfound"
         style={{ height: "100vh", width: "100vw", objectFit: "contain" }}
-      />
+      /> */}
     </div>
   );
 };
