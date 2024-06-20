1f6468aa15c2151de46106c30e04cab1c0edd69c
Akash581999 <akash581999@gmail.com>
2024-06-20 20:24:35 +0530

    Admin page added

---
+++ src/components/Admin.jsx
@@ -0,0 +1,319 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import AllTraxslogo from "../images/AllTraxslogo.png";
+import user from "../images/userimg1.jpg";
+// import SearchSongs from "./SearchSongs";
+import SongPlayer from "./SongPlayer";
+// import Album from "./Album";
+import LikedSongs from "./LikedSongs";
+// import SongsLibrary from "./SongsLibrary";
+
+const Admin = (props) => {
+    return (
+        <>
+            <div className={`bg-${props.mode}`}>
+                <nav className="navbar sticky-top navbar-expand-lg navbar-dark bg-dark z-1">
+                    <div className="container-fluid">
+                        <div className="navbar-brand d-flex align-items-center mx-auto">
+                            <img src={AllTraxslogo} alt="logo" className="mx-1 my-1" />
+                            <Link className="navbar-brand" to="/DashBoardScreen">
+                                <span
+                                    style={{
+                                        color: "#8b08ff",
+                                        fontWeight: "bolder",
+                                        fontSize: "36px",
+                                    }}
+                                >
+                                    AllTraxs
+                                </span>
+                            </Link>
+                        </div>
+                        <button
+                            className="navbar-toggler"
+                            type="button"
+                            data-bs-toggle="collapse"
+                            data-bs-target="#navbarSupportedContent"
+                            aria-controls="navbarSupportedContent"
+                            aria-expanded="false"
+                            aria-label="Toggle navigation"
+                        >
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="search navbar-nav ms-5 w-100">
+                            <SongPlayer />
+                        </div>
+                        <div
+                            className="collapse navbar-collapse"
+                            id="navbarSupportedContent"
+                        >
+                            <div className="navbar-nav text-end d-flex flex-row my-3 justify-content-center align-items-center">
+                                <div className="form-check form-switch text-light mx-2">
+                                    <input
+                                        onClick={props.toggleMode}
+                                        className="form-check-input"
+                                        type="checkbox"
+                                        role="switch"
+                                        id="flexSwitchCheckDefault"
+                                    />
+                                    <label
+                                        className="form-check-label text-wrap text-secondary"
+                                        htmlFor="flexSwitchCheckDefault"
+                                    >
+                                        {props.mode}mode
+                                    </label>
+                                </div>
+                                <button
+                                    className="btn btn-primary text-nowrap mx-2"
+                                    type="button"
+                                    data-bs-toggle="offcanvas"
+                                    data-bs-target="#offcanvasExample"
+                                    aria-controls="offcanvasExample"
+                                >
+                                    <i className="fa fa-list"></i>&nbsp;Menu
+                                </button>
+                                <div className="nav-item text-nowrap mx-2">
+                                    <Link to="/LoginScreen">
+                                        <button className="btn btn-danger" type="button">
+                                            <i className="fa fa-sign-out"></i>&nbsp;Log out
+                                        </button>
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+
+                <article>
+                    {/* Offcamvas starts here */}
+                    <div
+                        className="offcanvas offcanvas-end bg-dark"
+                        tabIndex="-1"
+                        id="offcanvasExample"
+                        aria-labelledby="offcanvasExampleLabel"
+                    >
+                        <div className="offcanvas-header d-flex justify-content-center">
+                            <Link
+                                to="/DashBoardScreen"
+                                className="my-2 d-flex align-items-center text-dark text-decoration-none"
+                            >
+                                <h5 className="offcanvas-title" id="offcanvasExampleLabel">
+                                    <span className="fs-1 text-light">Profile</span>
+                                </h5>
+                                <img
+                                    src={user}
+                                    alt="profile"
+                                    width="98"
+                                    height="98"
+                                    className="rounded-circle mx-2"
+                                />
+                            </Link>
+                            <button
+                                type="button"
+                                className="btn-close btn btn-outline-dark bg-danger"
+                                data-bs-dismiss="offcanvas"
+                                aria-label="Close"
+                            ></button>
+                        </div>
+                        <hr className="text-secondary" />
+                        <div className="offcanvas-body">
+                            <ul className="nav nav-pills flex-column mb-auto">
+                                <li className="nav-item">
+                                    <a
+                                        href="/DashBoardScreen"
+                                        className="nav-link active"
+                                        aria-current="page"
+                                    >
+                                        <i className="fa fa-home"></i>&nbsp; Home
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="/DashBoardScreen" className="nav-link text-light">
+                                        <i className="fa fa-dashboard"></i>&nbsp; Dashboard
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="/DashBoardScreen" className="nav-link text-light">
+                                        <i className="fa fa-search"></i>&nbsp; Search
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="/" className="nav-link text-light">
+                                        <i className="fa fa-list"></i>&nbsp; Playlists
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="/" className="nav-link text-light">
+                                        <i className="fa fa-heart"></i>&nbsp; Liked songs
+                                    </a>
+                                </li>
+                                <li>
+                                    <Link
+                                        to="/SubscriptionScreen"
+                                        className="nav-link text-light"
+                                    >
+                                        <i className="fa fa-money"></i>&nbsp; Get premium
+                                    </Link>
+                                </li>
+                            </ul>
+                            <hr className="text-secondary" />
+                            <div className="dropdown">
+                                <a
+                                    href="/"
+                                    className="d-flex align-items-center text-light text-decoration-none dropdown-toggle"
+                                    id="dropdownUser1"
+                                    data-bs-toggle="dropdown"
+                                    aria-expanded="false"
+                                >
+                                    <img
+                                        // src="https://github.com/mdo.png"
+                                        src={user}
+                                        alt="user"
+                                        width="32"
+                                        height="32"
+                                        className="rounded-circle me-2"
+                                    />
+                                    <strong>User</strong>
+                                </a>
+                                <ul
+                                    className="dropdown-menu dropdown-menu-dark text-small shadow"
+                                    aria-labelledby="dropdownUser1"
+                                >
+                                    <li>
+                                        <a className="dropdown-item" href="/">
+                                            <i className="fa fa-user"></i>&nbsp; Profile
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a className="dropdown-item" href="/">
+                                            <i className="fa fa-lock"></i>&nbsp;&nbsp;
+                                            {/* <i className="fa fa-key"></i>
+                        <i className="fa fa-bank"></i> */}
+                                            Account
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <Link className="dropdown-item" to="/ForgotScreen">
+                                            <i className="fa fa-wrench"></i>&nbsp;Settings
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <hr className="dropdown-divider" />
+                                    </li>
+                                    <li>
+                                        <button
+                                            className="btn btn-outline-dark bg-danger mx-3"
+                                            type="submit"
+                                        >
+                                            <Link to="/LoginScreen" className="dropdown-item">
+                                                {/* Log out <i className="fa fa-sign-out"></i> */}
+                                                <i className="fa fa-power-off"></i>&nbsp;Log out
+                                            </Link>
+                                        </button>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    {/* Offcanvas ends here */}
+                </article>
+
+                <section>
+                    <div className="row">
+                        <div className="col-lg-2 col-md-3 col-sm-4 col-12">
+                            <div className="d-flex align-items-start h-100 bg-dark">
+                                <nav
+                                    className="nav d-flex flex-column align-items-stretch pe-4 nav-pills p-2 w-100 h-100"
+                                    id="v-pills-tab"
+                                    role="tablist"
+                                    aria-orientation="vertical"
+                                >
+                                    <ul className="d-flex flex-column" role="tablist">
+                                        <li className="nav-item" role="presentation">
+                                            <button
+                                                className="nav-link active w-100 text-light"
+                                                id="home-tab"
+                                                data-bs-toggle="tab"
+                                                data-bs-target="#home"
+                                                type="button"
+                                                role="tab"
+                                                aria-controls="home"
+                                                aria-selected="true"
+                                            >
+                                                <i className="fa fa-home text-info"></i>&nbsp; Home
+                                            </button>
+                                        </li>
+                                        <li className="nav-item" role="presentation">
+                                            <button
+                                                className="nav-link w-100 text-light"
+                                                id="profile-tab"
+                                                data-bs-toggle="tab"
+                                                data-bs-target="#profile"
+                                                type="button"
+                                                role="tab"
+                                                aria-controls="profile"
+                                                aria-selected="false"
+                                            >
+                                                <i className="fa fa-search text-info"></i>&nbsp;Search
+                                            </button>
+                                        </li>
+                                        <hr className="text-secondary" />
+                                        <li className="nav-item" role="presentation">
+                                            <button
+                                                className="nav-link w-100 text-light"
+                                                id="messages-tab"
+                                                data-bs-toggle="tab"
+                                                data-bs-target="#messages"
+                                                type="button"
+                                                role="tab"
+                                                aria-controls="messages"
+                                                aria-selected="false"
+                                            >
+                                                <i className="fa fa-table text-info"></i>&nbsp;Library
+                                            </button>
+                                        </li>
+                                        <li className="nav-item" role="presentation">
+                                            <button
+                                                className="nav-link w-100 text-light"
+                                                id="settings-tab"
+                                                data-bs-toggle="tab"
+                                                data-bs-target="#settings"
+                                                type="button"
+                                                role="tab"
+                                                aria-controls="settings"
+                                                aria-selected="false"
+                                            >
+                                                <i className="fa fa-thumbs-up text-info"></i>
+                                                &nbsp;Liked songs
+                                            </button>
+                                        </li>
+                                        <li className="nav-item" role="presentation">
+                                            <button
+                                                className="nav-link w-100 text-light"
+                                                id="settings-tab"
+                                                data-bs-toggle="tab"
+                                                data-bs-target="#settings"
+                                                type="button"
+                                                role="tab"
+                                                aria-controls="settings"
+                                                aria-selected="false"
+                                            >
+                                                <i className="fa fa-music text-info"></i>&nbsp;Playlists
+                                            </button>
+                                        </li>
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                        <div className="col-lg-10 col-md-9 col-sm-8 col-12">
+                            {/* <Album /> */}
+                            <LikedSongs />
+                            {/* <SongsLibrary /> */}
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </>
+    );
+};
+
+export default Admin;
--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,122 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+// import Header from "../components/Header";
+import Admin from "../components/Admin";
+import Footer from "../components/Footer";
+import "../App.css";
 
 const AdminPage = () => {
+  let [mode, setMode] = useState(`light`); //Dark mode
+
+  let toggleMode = () => {
+    if (mode === "dark") {
+      setMode("light");
+    } else {
+      setMode("dark");
+    }
+  };
   return (
     <>
-      <div className="container">
-        <h2>Upload Song</h2>
-        <form
-          className="row gy-2 gx-3 align-items-center"
-          id="uploadForm"
-          action="http://localhost:5000/api/songs"
-          method="post"
-          enctype="multipart/form-data"
-        >
-          <div>
-            <label for="title">Title:</label>
-            <input type="text" id="title" name="title" required />
-          </div>
-          <div>
-            <label for="artist">Artist:</label>
-            <input type="text" id="artist" name="artist" required />
-          </div>
-          <div className="form-floating">
-            <textarea
-              className="form-control"
-              placeholder="Leave a comment here"
-              id="floatingTextarea"
-              name="description"
-              required
-            ></textarea>
-            <label for="floatingTextarea">Description</label>
-          </div>
-          <div>
-            <label for="formFileLg" className="form-label">
-              Choose a file:
-            </label>
-            <input
-              className="form-control form-control-lg"
-              id="formFileLg"
-              type="file"
-              name="songFile"
-              accept=".mp3,.wav,.ogg"
-              required
-            />
-          </div>
-          <div>
-            <button type="submit">Upload Song</button>
-          </div>
-        </form>
-
-        <h2>Search songs here</h2>
-        <label for="exampleDataList" className="form-label">
-          Datalist example
-        </label>
-        <input
-          className="form-control"
-          list="datalistOptions"
-          id="exampleDataList"
-          placeholder="Type to search..."
-        />
-        <datalist id="datalistOptions">
-          <option value="San Francisco"></option>
-          <option value="New York"></option>
-          <option value="Seattle"></option>
-          <option value="Los Angeles"></option>
-          <option value="Chicago"></option>
-        </datalist>
-
-        <form className="row row-cols-lg-auto g-3 align-items-center">
-          <div className="col-12">
-            <label
-              className="visually-hidden"
-              for="inlineFormInputGroupUsername"
-            >
-              Username
-            </label>
-            <div className="input-group">
-              <div className="input-group-text">@</div>
-              <input
-                type="text"
-                className="form-control"
-                id="inlineFormInputGroupUsername"
-                placeholder="Username"
-              />
-            </div>
-          </div>
-
-          <div className="col-12">
-            <label className="visually-hidden" for="inlineFormSelectPref">
-              Preference
-            </label>
-            <select className="form-select" id="inlineFormSelectPref">
-              <option selected>Choose...</option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
-            </select>
-          </div>
-
-          <div className="col-12">
-            <div className="form-check">
-              <input
-                className="form-check-input"
-                type="checkbox"
-                id="inlineFormCheck"
-              />
-              <label className="form-check-label" for="inlineFormCheck">
-                Remember me
-              </label>
-            </div>
-          </div>
-
-          <div className="col-12">
-            <button type="submit" className="btn btn-primary">
-              Submit
-            </button>
-          </div>
-        </form>
-      </div>
+      {/* <Header /> */}
+      <Admin mode={mode} toggleMode={toggleMode} />
+      <Footer />
     </>
   );
 };
